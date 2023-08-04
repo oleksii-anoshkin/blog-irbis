050c267afc7eb2f1e9469109bf2e50235b8aa526
Oleksii Anoshkin <anoshkinaleksey@gmail.com>
2023-08-04 16:55:44 +0300

    Configure React blog app

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { useState, useLayoutEffect, Fragment } from "react";
 
 import "./App.css";
@@ -36,7 +36,7 @@ export default function App() {
   return (
     <>
       <Theme isLight={isLight} parentClass={wrapperClass}>
-        <BrowserRouter>
+        <HashRouter>
           <Header
             parentClass={wrapperClass}
             linkIds={routesIds}
@@ -61,7 +61,7 @@ export default function App() {
             isLight={isLight}
             setIsLight={setIsLight}
           />
-        </BrowserRouter>
+        </HashRouter>
       </Theme>
     </>
   );
